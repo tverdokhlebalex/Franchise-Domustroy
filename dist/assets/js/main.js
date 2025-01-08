07c434addc4c609f76e81db1d756d136dad40fa3
Alexander <tverdokhlebalex@me.com>
2025-01-08 20:44:50 +0400

    up

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -6,17 +6,3 @@ AOS.init({
   duration: 1500, // Продолжительность анимации (в мс)
   offset: 150, // Смещение (в пикселях) перед активацией анимации
 });
-
-//ПРЕИМУЩЕСТВА
-// Складывание карточек для мобильных
-document.addEventListener("DOMContentLoaded", () => {
-  const cards = document.querySelectorAll(".advantage-card");
-
-  if (window.innerWidth <= 768) {
-    let zIndex = cards.length;
-
-    cards.forEach((card) => {
-      card.style.zIndex = zIndex--;
-    });
-  }
-});
